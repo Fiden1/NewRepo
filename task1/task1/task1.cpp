51c30f5a491f74d7e19c3109dc50d8d37de3644f
Fiden1 <74004023+Fiden1@users.noreply.github.com>
2022-01-29 17:08:48 +0200

    Добавьте файлы проекта.

---
+++ task1/task1/task1.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+class Calculator
+{
+public:
+    double calculate(char oper, double x, double y)
+
+    {
+        switch (oper)
+        {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case 's':
+            return sqrt(x);
+        case 'p':
+            return pow(x, y);
+        case '%':
+            return (y / 100) * x;
+        default:
+            return 0;
+        }
+
+    }
+
+    string operationName(char oper)
+    {
+        string n = "";
+        switch (oper)
+        {
+        case '+':
+            n = "addition";
+            break;
+        case '-':
+            n = "subtraction";
+            break;
+        case '*':
+            n = "multiplication";
+            break;
+        case 's':
+            n = "rooting";
+            break;
+        case 'p':
+            n = "pi";
+            break;
+        case '%':
+            n = "percentage of the number";
+            break;
+        default:
+            n = "return";
+            break;
+        }
+
+        return n;
+    }
+};
+double mainResult(int result, string d) {
+    if (d == "yes")
+        return fabs(result);
+    else {
+        return result;
+    }
+};
+
+int main()
+{
+    double x = 0.0;
+    double y = 0.0;
+    int result = 0.0;
+    char oper = '+';
+    string d = "yes";
+    string n = "";
+
+    cout << "need to use modulo values (yes ; no)" << endl;
+    cin >> d;
+    cout << "what operation do you want to perform(+ ; - ; * ; / ; s (sqrt); p (pow) ; % )" << endl;
+    cin >> oper;
+    if (d == "yes" || d == "no")
+    {
+        cout << "1 Number=" << endl;
+        cin >> x;
+        cout << "2 Number=" << endl;
+        cin >> y;
+        Calculator c;
+        if (oper == '/' && y == 0 || x == 0 && y == 0)
+        {
+            cout << "division by zero exception";
+        }
+        else {
+            result = c.calculate(x, oper, y);
+            n = c.operationName(oper);
+            if (d == "yes")
+            {
+                cout << n << "." << x << " " << oper << " " << y << "=" << mainResult(result, d) << endl;
+            }
+            else {
+                cout << n << "." << x << oper << y << "=" << mainResult(result, d) << endl;
+            }
+        }
+    }
+    else { cout << "error: enter yes or no"; };
+}
---
+++ task2/task2/task2.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+using namespace std;
+
+int customRandom(int min, int max)
+{
+	return min + rand() % (max - min + 1);
+}
+int win(int sum, int compSum) {
+	if (sum > compSum)
+		cout << "you win with " << sum << " point,computer got " << compSum << endl;
+	if (sum == compSum) {
+		cout << "it's a draw,you got " << sum << " points,computer got " << compSum << endl;
+	}
+	else {
+		cout << "you lose with " << sum << " point,computer got " << compSum << endl;
+	}
+}
+int main(){
+	string want = "";
+	string next = "";
+	if (customRandom(1, 2) == 1) {
+		cout << "you first,want to roll the dice?(yes or no)" << endl;
+		cin >> want;
+		if (want == "yes") {
+			cout << " u throw the dice "<<endl;
+			int firstThrow = customRandom(1, 6);
+			cout << " you got " << firstThrow << " points " << endl;
+			int secondThrow = customRandom(1, 6);
+			int intermediateResult = firstThrow + secondThrow;
+			cout << " you got " << firstThrow << " and " << secondThrow << " points " << endl;
+			cout << " computer throw the dice " << endl;
+			int firstCompThrow = customRandom(1, 6);
+			cout << " Computer got " << firstCompThrow << " points " << endl;
+			int secondCompThrow = customRandom(1, 6);
+			int intermediateCompResult = firstCompThrow + secondCompThrow;
+			cout << " you got " << firstThrow << " and " << secondThrow << " points " << endl;
+			cout << " will you throw more(yes or no) " << endl;
+			cin >> next;
+			if (next == "yes") {
+				cout << " u throw the dice " << endl;
+				int thirdThrow = customRandom(1, 6);
+				cout <<  " you got " << thirdThrow << " points " << endl;
+				int fourthThrow = customRandom(1, 6);
+				int sum = intermediateResult + thirdThrow + fourthThrow;
+				int thirdCompThrow = customRandom(1, 6);
+				cout << " Computer got " << firstCompThrow << " points " << endl;
+				int fourthCompThrow = customRandom(1, 6);
+				int compSum = intermediateCompResult + thirdCompThrow + fourthCompThrow;
+				cout << win(sum, compSum) << endl;
+
+			}
+			else {
+				cout << "why not ? ): ";
+			}
+		}
+		else {
+			cout << "why not? ):";
+		}
+	}
+	else {
+		cout << " computer throw the dice ";
+		int firstCompThrow = customRandom(1, 6);
+		cout << " Computer got " << firstCompThrow << " points " << endl;
+		int secondCompThrow = customRandom(1, 6);
+		int intermediateCompResult = firstCompThrow + secondCompThrow;
+		cout << " Comp got " << firstCompThrow << " and " << secondCompThrow << " points " << endl;
+		cout << " u throw the dice " << endl;
+		int firstThrow = customRandom(1, 6);
+		cout << " you got " << firstThrow << " points " << endl;
+		int secondThrow = customRandom(1, 6);
+		int intermediateResult = firstThrow + secondThrow;
+		cout << " you got " << firstThrow << " and " << secondThrow << " points " << endl;
+		
+		cout << "will you throw more(yes or no)" << endl << endl;
+		cin >> next;
+		if (next == "yes") {
+			cout << " u throw the dice " << endl;
+			int thirdThrow = customRandom(1, 6);
+			cout << " you got " << thirdThrow << " points " << endl;
+			int fourthThrow = customRandom(1, 6);
+			int sum = intermediateResult + thirdThrow + fourthThrow;
+			int thirdCompThrow = customRandom(1, 6);
+			cout << " Computer got " << firstCompThrow << " points " << endl;
+			int fourthCompThrow = customRandom(1, 6);
+			int compSum = intermediateCompResult + thirdCompThrow + fourthCompThrow;
+			cout << win(sum, compSum) << endl;
+		}
+		else {
+			cout << "why not? ):";
+		}
+	}
+}
